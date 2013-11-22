c003a5db56213da05bc6937155533926eed532c7
dhlwing <249717835@qq.com>
2013-11-22 14:14:24 +0800

    add usage

--- config.sample.php
+++ config.sample.php
@@ -3,7 +3,7 @@
 define('DEBUG',1);
 define('API_KEY','bzRaB8Fy93xW4cNFbWFuMIPT');
 define('SEC_KEY','rKFg2LEmXApkMX5NkQaWcSE9LIHO4ZS6');
-//根据自己获取的值填写
+//根据自己获取的值填写 也可以空着使用init参数初始化来获取
 define('ACCESS_TOKEN','');
 define('REFRESH_TOKEN','');
 
